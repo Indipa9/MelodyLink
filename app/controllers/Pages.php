4e033c2932782737cb8230c194fdd681b83425fc
Indipa9 <IndipaPerera1@gmail.com>
2024-12-08 11:25:28 +0530

    Framework designed for MelodyLink

---
+++ app/controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+class Pages extends Controller{
+public function __construct(){
+   // echo 'this is the pages controller';
+$this->pagesModel=$this->model('m_Pages');
+}
+public function index(){
+
+
+
+}
+public function about(){
+    $users=$this->pagesModel->getUsers();
+$data=[
+    'users'=>$users
+];
+
+$this->view('v_about',$data);
+
+}
+
+
+}
+?>
